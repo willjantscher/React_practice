35d9bffd2d36890e4d5418177cf5de11f0e27fdd
willjantscher <william.jantscher@gmail.com>
2020-11-04 13:44:47 -0700

    stuff

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+
+// cd learning-react/src
+// npm start
+
+// Change the header on this site from ‘React App’ (seen in the browser bar) to “Grocery Shopping App”
+// Get rid of the spinning image on the page, and the ‘Edit src/App.js…’ and replace it with a heading that indicates this is now a grocery list application! 
+// Hard-code 3 grocery items (of your choosing). I’d like them to be formatted as a list of bullet points underneath the page heading
+// BONUS: Change the favicon visible in your browser tab to an image relevant to grocery shopping!
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="Grocery Shopping App">
+              <h1>This is now a grocery list application!</h1>
+              <ul>
+                  <li>Eggs</li>
+                  <li>Beer</li>
+                  <li>Soup</li>
+              </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
